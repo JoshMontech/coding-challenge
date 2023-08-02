f5d45f1a2f183562438931abf99a4074b32cbd0a
Josh <JoshMontech@users.noreply.github.com>
2023-08-02 01:21:19 -0500

    finished project and added easter egg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google"
+import { ApolloWrapper } from "@/lib/apollo-provider"
 import "./globals.css"
 import Providers from "./providers"
 import MainHeader from "@/components/MainHeader"
@@ -14,8 +15,10 @@ export default function RootLayout({
         <html lang="en" className={inter.className}>
             <body className="relative h-[v00vh] min-h-[100vh] max-w-[100vw] bg-primary text-[100%] text-primary">
                 <Providers>
-                    <MainHeader />
-                    {children}
+                    <ApolloWrapper>
+                        <MainHeader />
+                        {children}
+                    </ApolloWrapper>
                 </Providers>
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,56 @@
+"use client"
 import Image from "next/image"
 import splash from "../public/splash.svg"
+import { useState } from "react"
+import { gql, useMutation } from "@apollo/client"
+import { MUTATION_CREATE_NEW_USER } from "@/lib/graphql/mutationCreateNewUser"
+const CREATE_NEW_USER = gql(MUTATION_CREATE_NEW_USER)
+import { useTheme } from "next-themes"
 
 export default function Home() {
+    const { theme, setTheme } = useTheme()
+    const [createNewUser] = useMutation(CREATE_NEW_USER)
+    const [email, setEmail] = useState("")
+    const [toastBar, setToastBar] = useState({ message: "", type: "error" })
+    const validateEmail = (email) => {
+        var re = /\S+@\S+\.\S+/
+        return re.test(email)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (!validateEmail(email)) {
+            setToastBar({
+                message: "Please enter a real email",
+                type: "error",
+            })
+            return
+        } else console.log(email)
+        createNewUser({
+            variables: {
+                name: event.target.email.value,
+                username: event.target.email.value,
+                email: event.target.email.value,
+            },
+        })
+            .then((res) => {
+                setToastBar({
+                    message: `successfully created`,
+                    type: `success`,
+                })
+                setEmail("")
+                console.log(res.data)
+            })
+            .catch((error) => console.error(error.message))
+    }
     return (
         <main
-            className="mx-auto flex h-full w-full max-w-[1280px] flex-col px-[24px]"
+            className="relative z-10 mx-auto flex h-full w-full max-w-[1280px] flex-col px-[24px]"
             style={{ maxHeight: "calc(100vh-90px)" }}
         >
             <section className="relative flex h-full">
                 <div className="relative mt-[24px] flex flex-col gap-[24px] md:mt-[100px] lg:mt-[125px]">
                     <a
-                        className="flex items-center gap-[16px] self-start rounded-[14px] bg-black py-[4px] pl-[4px] pr-[8px] text-[12px] leading-[20px]"
+                        className="flex items-center gap-[16px] self-start rounded-[14px] bg-tertiary py-[4px] pl-[4px] pr-[8px] text-[12px] leading-[20px]"
                         href="/"
                     >
                         <div className="rounded-[10px] bg-secondary px-[12px] py-[2px] uppercase">
@@ -35,20 +75,38 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="mt-[24px] flex max-w-[509px] flex-col gap-[16px]">
-                        <form>
-                            <div className="flex w-full flex-col gap-[12px] sm:flex-row">
-                                <input
-                                    className="w-full rounded-[6px] px-[16px] py-[12px] sm:w-[300px] md:w-[350px]"
-                                    type="email"
-                                    name="email"
-                                    placeholder="Enter your email"
-                                />
-                                <button
-                                    type="submit"
-                                    className="rounded-[6px] bg-secondary px-[16px] py-[12px]"
-                                >
-                                    Start free trial
-                                </button>
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <div className="flex w-full flex-col gap-[12px]">
+                                {toastBar.message !== "" && (
+                                    <div
+                                        className={`rounded-[4px] px-[16px] py-[12px] text-center md:self-start ${
+                                            toastBar.type === "error"
+                                                ? "border-red-500 bg-red-200 text-red-500"
+                                                : "border-[2px] border-green-500 bg-green-200 text-green-500"
+                                        }`}
+                                    >
+                                        {toastBar.message}
+                                    </div>
+                                )}
+                                <div className="flex flex-col gap-[12px] sm:flex-row">
+                                    <input
+                                        className="w-full rounded-[6px] px-[16px] py-[12px] text-tertiary sm:w-[300px] md:w-[350px]"
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                        placeholder="Enter your email"
+                                    />
+                                    <button
+                                        type="submit"
+                                        className="rounded-[6px] bg-secondary px-[16px] py-[12px]"
+                                    >
+                                        Start free trial
+                                    </button>
+                                </div>
                             </div>
                         </form>
                         <div className="text-[14px] text-sm leading-[20px] text-secondary">
@@ -58,7 +116,7 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="fixed bottom-[-100px] right-0 z-[-10] md:bottom-auto md:right-[-275px] lg:right-[-125px] max:right-0">
+                <div className="fixed bottom-[-100px] right-0 z-[-10] md:bottom-auto md:right-[-275px] lg:right-[-125px] max:absolute max:right-0">
                     <Image
                         src={splash}
                         height={625}
